32d84004fdd0a50c7481b2be991d9ed3c9ab46e8
GejjalaLakshmipathi <85888123+GejjalaLakshmipathi@users.noreply.github.com>
2022-11-21 12:12:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,27 +7,28 @@ import matplotlib.pyplot as plt
 
 # col1, col2 = st.columns([1, 3])
 
-st.title('Phishing Website Detection using Machine Learning')
+st.title('Implement a deep learning based system that detects whether a website is fake.
+')
 st.write('This ML-based app is developed for educational purposes. Objective of the app is detecting phishing websites only using content data. Not URL!'
          ' You can see the details of approach, data set, and feature set if you click on _"See The Details"_. ')
 
 
 with st.expander("PROJECT DETAILS"):
     st.subheader('Approach')
-    st.write('I used _supervised learning_ to classify phishing and legitimate websites. '
-             'I benefit from content-based approach and focus on html of the websites. '
-             'Also, I used scikit-learn for the ML models.'
+    st.write('We have used _supervised learning_ to classify phishing and legitimate websites. '
+             'It benefit from content-based approach and we firstly focus on html of the websites. '
+             'And also, we used scikit-learn for the ML models.'
              )
-    st.write('For this educational project, '
-             'I created my own data set and defined features, some from the literature and some based on manual analysis. '
-             'I used requests library to collect data, BeautifulSoup module to parse and extract features. ')
+    st.write('For this project, '
+             'We created our own data set and defined features, some from the literature and some based on manual analysis. '
+             'we used requests library to collect data, BeautifulSoup module to parse and extract features. ')
     st.write('The source code and data sets are available in the below Github link:')
-    st.write('_https://github.com/emre-kocyigit/phishing-website-detection-content-based_')
+    st.write('_https://github.com/GejjalaLakshmipathi/Phishing_Website_Detection_')
 
     st.subheader('Data set')
-    st.write('I used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
+    st.write('We have used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
     st.write('Totally 26584 websites ==> **_16060_ legitimate** websites | **_10524_ phishing** websites')
-    st.write('Data set was created in October 2022.')
+    st.write('Data set was created in November 2022.')
 
     # ----- FOR THE PIE CHART ----- #
     labels = 'phishing', 'legitimate'
@@ -62,11 +63,11 @@ with st.expander("PROJECT DETAILS"):
     )
 
     st.subheader('Features')
-    st.write('I used only content-based features. I didn\'t use url-based faetures like length of url etc.'
+    st.write('Firstly, we have used only content-based features. Still we didn\'t use url-based faetures like length of url etc.'
              'Most of the features extracted using find_all() method of BeautifulSoup module after parsing html.')
 
     st.subheader('Results')
-    st.write('I used 7 different ML classifiers of scikit-learn and tested them implementing k-fold cross validation.'
+    st.write('We have used 7 different ML classifiers of scikit-learn and tested them implementing k-fold cross validation.'
              'Firstly obtained their confusion matrices, then calculated their accuracy, precision and recall scores.'
              'Comparison table is below:')
     st.table(ml.df_results)
@@ -78,11 +79,11 @@ with st.expander("PROJECT DETAILS"):
     st.write('NN --> Neural Network')
     st.write('KN --> K-Neighbours')
 
-with st.expander('EXAMPLE PHISHING URLs:'):
+with st.expander('LETS CHECK WITH SOME OF THE PHISHING URLs:'):
     st.write('_https://rtyu38.godaddysites.com/_')
     st.write('_https://karafuru.invite-mint.com/_')
     st.write('_https://defi-ned.top/h5/#/_')
-    st.caption('REMEMBER, PHISHING WEB PAGES HAVE SHORT LIFECYCLE! SO, THE EXAMPLES SHOULD BE UPDATED!')
+    st.caption('REMEMBER, PHISHING WEB PAGES HAVE SHORT LIFECYCLE! SO, THE EXAMPLES HERE MENTIONED ARE GIVEN IN DATASET AND EXAMPLES SHOULD BE UPDATED AS WE UPDATE DATASET!')
 
 choice = st.selectbox("Please select your machine learning model",
                  [
